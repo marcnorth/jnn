7726fb99814fbf749fae930e362242715d144d07
Marc North <contact@marcnorth.co.uk>
2020-03-29 20:56:30 +0100

    Created GeneticAlgorithm class

---
+++ src/uk/co/marcnorth/jnn/GeneticAlgorithm.java
@@ -0,0 +1,11 @@
+package uk.co.marcnorth.jnn;
+
+public class GeneticAlgorithm {
+	
+	private int currentGeneration;
+	private NeuralNetwork[] networks;
+	private NeuralNetworkTask task;
+	
+	
+	
+}
---
+++ src/uk/co/marcnorth/jnn/NeuralNetworkTask.java
@@ -0,0 +1,7 @@
+package uk.co.marcnorth.jnn;
+
+public interface NeuralNetworkTask {
+	
+	public void runTask(NeuralNetwork network);
+	
+}
